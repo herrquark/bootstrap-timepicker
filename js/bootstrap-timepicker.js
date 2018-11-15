3f38172adaccb5bac668a79765128eca35f1e698
quark <herrquark@gmail.com>
2018-11-15 15:15:59 +0200

    remind myself what version this is

--- js/bootstrap-timepicker.js
+++ js/bootstrap-timepicker.js
@@ -7,6 +7,8 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * Current repo URL https://github.com/herrquark/bootstrap-timepicker
  */
 (function($, window, document) {
   'use strict';
